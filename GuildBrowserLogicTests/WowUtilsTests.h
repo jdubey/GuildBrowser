15c11c482fe39c0591227c85a53664d8720c9133
Charlie Fulton <charlie@fulton>
2012-09-07 00:26:23 -0400

    initial checkin

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Charlie Fulton on 8/26/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
