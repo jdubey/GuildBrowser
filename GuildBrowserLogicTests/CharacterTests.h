8b8f968e80d9166a4e9cbf3059f257908e9562f1
Josh Dubey <joshua.dubey@gmail.com>
2012-11-19 13:56:23 -0800

    more tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Joshua Dubey on 11/19/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
